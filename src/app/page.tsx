edeb13a3aedaafd1ceaf71e9229e614d182c33be
STEVE\ttran <tttranduy999@gmail.com>
2025-09-06 10:53:02 +0700

    update revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
-import { client } from '@/lib/sanity'
-import { aboutQuery, projectsQuery } from '@/lib/queries'
-import { About, Project } from '@/types'
-import Hero from '@/components/sections/Hero'
-import AboutPreview from '@/components/sections/AboutPreview'
-import ProjectsGrid from '@/components/sections/ProjectsGrid'
+import { client } from "@/lib/sanity";
+import { aboutQuery, projectsQuery } from "@/lib/queries";
+import { About, Project } from "@/types";
+import Hero from "@/components/sections/Hero";
+import AboutPreview from "@/components/sections/AboutPreview";
+import ProjectsGrid from "@/components/sections/ProjectsGrid";
+
+// Thêm revalidate cho trang chủ
+export const revalidate = 60;
 
 export default async function Home() {
+  // Sử dụng Promise.all để fetch cả about và projects song song
   const [about, projects] = await Promise.all([
-    client.fetch<About>(aboutQuery),
-    client.fetch<Project[]>(projectsQuery)
-  ])
+    client.fetch<About>(aboutQuery, {}, { next: { revalidate: 60 } }),
+    client.fetch<Project[]>(projectsQuery, {}, { next: { revalidate: 60 } })
+  ]);
 
   return (
     <main>
@@ -17,5 +21,5 @@ export default async function Home() {
       <AboutPreview about={about} />
       <ProjectsGrid projects={projects || []} limit={3} />
     </main>
-  )
+  );
 }
